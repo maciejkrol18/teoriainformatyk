d32526ff7fb622a49dba49382d18da871f7b1384
Maciej Król <maciekgplay@gmail.com>
2023-08-12 21:06:00 +0200

    Added mobile navigation menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,31 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { Menu } from "lucide-react"
+import { XCircleIcon, Menu } from "lucide-react"
+import { useState } from "react"
+import MobileNavigation from "./MobileNavigation"
 
 interface HeaderProps {
   transparent?: boolean
 }
 
 export default function Header({ transparent }: HeaderProps) {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false)
   return (
     <header className={cn({ "bg-primary": transparent }, "py-4 sticky")}>
-      <div className="container mx-auto flex justify-between items-center">
-        <span className="text-2xl font-bold tracking-wide">teoriainformatyk</span>
+      <div className="px-4 md:px-0 container mx-auto flex justify-between items-center">
+        <a href="/" className="text-2xl font-bold tracking-wide">
+          teoriainformatyk
+        </a>
         {/* Mobile nav menu placeholder */}
-        <Menu className="h-full aspect-square lg:hidden" />
+        <button onClick={() => setMobileNavOpen((prev) => !prev)}>
+          {mobileNavOpen ? (
+            <XCircleIcon className="h-full aspect-square" />
+          ) : (
+            <Menu className="h-full aspect-square" />
+          )}
+        </button>
+        {mobileNavOpen && <MobileNavigation />}
       </div>
     </header>
   )
