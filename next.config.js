f7622bb155e20ea95c8e8573daca856bb0f3ed21
Maciej Król <maciekgplay@gmail.com>
2023-08-13 12:39:52 +0200

    Redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/inf02',
+        destination: '/inf02/jedno-pytanie',
+        permanent: true,
+      },
+      {
+        source: '/inf03',
+        destination: '/inf03/jedno-pytanie',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
