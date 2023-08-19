f6c21ee8239edd545abc36356740eb616825dfcd
Maciej Król <maciekgplay@gmail.com>
2023-08-19 11:45:07 +0200

    Added question count check
    Previously the count of questions used to roll a random ID was a hardcoded value. Now, the code waits for the database to return the amount of table rows before rolling the initial random question (table row)

--- src/components/OneQuestion.tsx
+++ src/components/OneQuestion.tsx
@@ -4,37 +4,45 @@ import { supabase } from "@/lib/supabase"
 import * as React from "react"
 import Card from "./Card"
 import { Question } from "@/types/question"
-import { Database } from "@/types/database"
+import { Table } from "@/types/table"
 import { cn } from "@/lib/utils"
 import CardSkeleton from "./skeletons/CardSkeleton"
 
 interface OneQuestionProps {
-  database: keyof Database["public"]["Tables"]
+  table: Table
 }
 
-export default function OneQuestion({ database }: OneQuestionProps) {
+export default function OneQuestion({ table }: OneQuestionProps) {
   const [currentQuestion, setCurrentQuestion] = React.useState<Question | null>(null)
   const [selectedAnswer, setSelectedAnswer] = React.useState<string | null>(null)
+  const [questionCount, setQuestionCount] = React.useState<number | null>(null)
   const rollButtonRef = React.useRef<HTMLButtonElement | null>(null)
 
   const getRandomQuestion = async () => {
-    const amount = database === "questions_inf02" ? 2249 : 1043
-    const randomId = Math.round(Math.random() * (amount - 1) + 1)
+    if (questionCount) {
+      const randomId = Math.round(Math.random() * (questionCount - 1) + 1)
 
-    const { data, error } = await supabase
-      .from(database)
-      .select("answers, content, correct_answer, id, image")
-      .eq("id", randomId)
+      const { data, error } = await supabase
+        .from(table)
+        .select("answers, content, correct_answer, id, image")
+        .eq("id", randomId)
 
-    if (error) {
-      return error
+      if (error) {
+        return error
+      }
+
+      if (data[0] === undefined) {
+        throw new Error("Returned question is undefined")
+      }
+
+      setCurrentQuestion(data[0])
     }
+  }
 
-    if (data[0] === undefined) {
-      throw new Error("Returned question is undefined")
-    }
-
-    setCurrentQuestion(data[0])
+  const getQuestionCount = async (table: Table) => {
+    const { count, error } = await supabase.from(table).select("id", { count: "exact" })
+    if (error) return error
+    setQuestionCount(count)
   }
 
   const handleSpacebar = (e: KeyboardEvent) => {
@@ -55,12 +63,18 @@ export default function OneQuestion({ database }: OneQuestionProps) {
   }
 
   React.useEffect(() => {
-    getRandomQuestion()
+    getQuestionCount(table)
 
     window.addEventListener("keydown", handleSpacebar)
 
     return () => window.removeEventListener("keydown", handleSpacebar)
   }, [])
+
+  React.useEffect(() => {
+    if (questionCount) {
+      rollQuestion()
+    }
+  }, [questionCount])
 
   return (
     <main className="flex flex-col gap-6 pb-8">
@@ -73,41 +87,43 @@ export default function OneQuestion({ database }: OneQuestionProps) {
         <span className="hidden lg:inline">(Spacja)</span>
       </button>
       {currentQuestion ? (
-        <Card>
-          <div className="flex flex-col gap-2">
-            <span className="text-secondary-300">#{currentQuestion.id}</span>
-            <h1 className="text-lg font-semibold">{currentQuestion.content}</h1>
-          </div>
-          <div className="flex flex-col gap-2">
-            {currentQuestion.answers.map((answer, idx) => {
-              const letters = "abcd"
-              return (
-                <button
-                  onClick={() => submitAnswer(answer)}
-                  disabled={Boolean(selectedAnswer)}
-                  className={cn(
-                    "flex gap-2 bg-secondary-300 p-2 drop-shadow-lg",
-                    {
-                      "bg-positive-light":
-                        selectedAnswer && answer === currentQuestion.correct_answer,
-                    },
-                    {
-                      "bg-danger-light":
-                        selectedAnswer &&
-                        selectedAnswer === answer &&
-                        answer !== currentQuestion.correct_answer,
-                    },
-                  )}
-                  key={idx}
-                >
-                  <span className="uppercase font-semibold">{letters.at(idx)}.</span>
-                  <span className="text-left">{answer}</span>
-                </button>
-              )
-            })}
-          </div>
-          {currentQuestion.image && <img src={currentQuestion.image}></img>}
-        </Card>
+        <>
+          <Card>
+            <div className="flex flex-col gap-2">
+              <span className="text-secondary-300">#{currentQuestion.id}</span>
+              <h1 className="text-lg font-semibold">{currentQuestion.content}</h1>
+            </div>
+            <div className="flex flex-col gap-2">
+              {currentQuestion.answers.map((answer, idx) => {
+                const letters = "abcd"
+                return (
+                  <button
+                    onClick={() => submitAnswer(answer)}
+                    disabled={Boolean(selectedAnswer)}
+                    className={cn(
+                      "flex gap-2 bg-secondary-300 p-2 drop-shadow-lg",
+                      {
+                        "bg-positive-light":
+                          selectedAnswer && answer === currentQuestion.correct_answer,
+                      },
+                      {
+                        "bg-danger-light":
+                          selectedAnswer &&
+                          selectedAnswer === answer &&
+                          answer !== currentQuestion.correct_answer,
+                      },
+                    )}
+                    key={idx}
+                  >
+                    <span className="uppercase font-semibold">{letters.at(idx)}.</span>
+                    <span className="text-left">{answer}</span>
+                  </button>
+                )
+              })}
+            </div>
+            {currentQuestion.image && <img src={currentQuestion.image}></img>}
+          </Card>
+        </>
       ) : (
         <CardSkeleton />
       )}
