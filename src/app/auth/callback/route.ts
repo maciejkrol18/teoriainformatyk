fe1a2287e4d7ff80c010a9c04a026f82f0d99f3e
Maciej Król <maciekgplay@gmail.com>
2024-04-07 12:13:25 +0200

    Error handling in auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -30,6 +30,8 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
       return NextResponse.redirect(`${origin}${next}`)
+    } else {
+      NextResponse.redirect(`${origin}/auth/auth-code-error/${JSON.stringify(error)}`)
     }
   }
 
