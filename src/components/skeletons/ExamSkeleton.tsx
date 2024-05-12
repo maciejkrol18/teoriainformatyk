9713c8ba3ab8a71e24edcfce7e933c3711877962
Maciej Król <maciekgplay@gmail.com>
2024-05-12 15:40:51 +0200

    refactor: exam skeleton responsiveness and sizes

--- src/components/skeletons/ExamSkeleton.tsx
+++ src/components/skeletons/ExamSkeleton.tsx
@@ -1,16 +1,19 @@
 export default function ExamSkeleton() {
   return Array.from({ length: 10 }).map((_, index) => (
-    <div className="flex flex-col gap-8 bg-background-light p-4 rounded-lg" key={index}>
-      <div className="bg-gradient-loading animate-loading bg-size-loading rounded-full p-4 w-12 h-12"></div>
-      <div className="bg-gradient-loading animate-loading bg-size-loading w-full h-6"></div>
-      <div className="bg-gradient-loading animate-loading bg-size-loading w-[75%] h-6"></div>
-      <div className="flex flex-col gap-4">
-        <div className="h-6 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
-        <div className="h-6 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
-        <div className="h-6 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
-        <div className="h-6 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+    <div className="flex flex-col gap-8 md:w-full md:max-w-xl md:mx-auto">
+      <div className="h-6 w-[34ch] mx-auto rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+      <div className="flex flex-col gap-8 bg-background-light p-4 rounded-lg" key={index}>
+        <div className="bg-gradient-loading animate-loading bg-size-loading rounded-full p-4 w-10 h-10"></div>
+        <div className="bg-gradient-loading animate-loading bg-size-loading w-full h-6"></div>
+        <div className="bg-gradient-loading animate-loading bg-size-loading w-[75%] h-6"></div>
+        <div className="flex flex-col gap-4">
+          <div className="h-10 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+          <div className="h-10 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+          <div className="h-10 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+          <div className="h-10 rounded-md bg-gradient-loading animate-loading bg-size-loading"></div>
+        </div>
+        <div className="h-[200px] bg-gradient-loading animate-loading bg-size-loading"></div>
       </div>
-      <div className="h-[200px] bg-gradient-loading animate-loading bg-size-loading"></div>
     </div>
   ))
 }
