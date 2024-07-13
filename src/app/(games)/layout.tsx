2d102f9f2b60b148c8160c2f7950780ca3473400
Maciej Król <maciekgplay@gmail.com>
2024-07-13 17:46:18 +0200

    fix: no space at the bottom

--- src/app/(games)/layout.tsx
+++ src/app/(games)/layout.tsx
@@ -4,7 +4,7 @@ export default function GamesLayout({ children }: { children: React.ReactNode })
   return (
     <>
       <Header />
-      <div className="container mx-auto flex flex-col grow gap-4">{children}</div>
+      <div className="container mx-auto flex flex-col grow gap-4 py-4">{children}</div>
     </>
   )
 }
