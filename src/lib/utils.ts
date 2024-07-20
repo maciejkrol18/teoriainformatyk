99ec7abb0f2e2f8ac74c7efa5cd46eacadecdc31
Maciej Król <maciekgplay@gmail.com>
2024-07-20 14:23:01 +0200

    fix: redirect fix?

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -45,7 +45,7 @@ export function declension(amount: number, a: string, b: string, c: string) {
 export function getURL() {
   let url =
     process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000/"
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`
