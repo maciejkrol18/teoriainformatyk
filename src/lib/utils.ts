c4e28b9e48fde786df9789ad1acee4708620b2ea
Maciej Król <maciekgplay@gmail.com>
2024-07-20 14:11:01 +0200

    refactor: get url function
    Updated snippet from supabase docs

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -44,10 +44,12 @@ export function declension(amount: number, a: string, b: string, c: string) {
 
 export function getURL() {
   let url =
-    process?.env?.NEXT_PUBLIC_SITE_URL ??
-    process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-    "http://localhost:3000"
-  url = url.includes("http") ? url : `https://${url}`
-  url = url.charAt(url.length - 1) === "/" ? url : `${url}/`
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "http://localhost:3000/"
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`
   return url
 }
