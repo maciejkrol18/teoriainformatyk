7e504a63192200d0de687930c13f95110b155053
Maciej Król <maciekgplay@gmail.com>
2024-07-20 14:28:41 +0200

    test: getting the url

--- src/components/auth/OAuthButtons.tsx
+++ src/components/auth/OAuthButtons.tsx
@@ -20,6 +20,24 @@ export default function OAuthButtons() {
     }
   }
 
+  function urlTest() {
+    console.log("NEXT_PUBLIC_SITE_URL", process.env.NEXT_PUBLIC_SITE_URL)
+    console.log("VERCEL_URL", process.env.VERCEL_URL)
+    console.log("NEXT_PUBLIC_VERCEL_URL", process.env.NEXT_PUBLIC_VERCEL_URL)
+    let url =
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+      process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
+      "http://localhost:3000/"
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith("http") ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith("/") ? url : `${url}/`
+    console.log("PROCESSED URL", url)
+    return url
+  }
+
+  urlTest()
+
   return (
     <div className="flex justify-center gap-4">
       <Button
