122fd2626a2255072d79e12a6b623c4fe3e8fb4c
Maciej Król <maciekgplay@gmail.com>
2024-08-19 13:28:45 +0200

    refactor: mobile navigation markup and styling

--- src/components/ui/MobileNavigation.tsx
+++ src/components/ui/MobileNavigation.tsx
@@ -73,78 +73,76 @@ export default function MobileNavigation({ user }: MobileNavigationProps) {
         {isOpen ? <XCircle /> : <Menu />}
       </button>
       {isOpen ? (
-        <nav className="bg-background-light z-50 fixed left-0 right-0 bottom-0 top-[67px]">
-          <div className="container mx-auto flex flex-col gap-6 py-4 max-h-[500px] overflow-scroll">
-            <div className="border border-background-bright bg-background-bright/30 flex flex-col justify-center gap-4 p-4">
-              {profile ? (
-                <ProfileBlock profile={profile} />
-              ) : (
-                <Button
-                  variant="primary"
-                  asChild
-                  /* 
+        <nav className="bg-background z-50 fixed inset-0 top-[67px] flex flex-col gap-4 overflow-y-scroll p-4">
+          <div className="border border-background-bright bg-background-bright/30 flex flex-col justify-center gap-4 p-4">
+            {profile ? (
+              <ProfileBlock profile={profile} />
+            ) : (
+              <Button
+                variant="primary"
+                asChild
+                /* 
                     MobileNavigation is not present in the auth layout
                     so we need to set the overflow to auto manually
                   */
-                  onClick={() => (document.body.style.overflow = 'auto')}
-                >
-                  <Link href="/login">Zaloguj</Link>
-                </Button>
-              )}
-            </div>
-            <Link href="/" className="text-xl pb-2" onClick={() => closeOnCurrent('/')}>
-              Strona główna
-            </Link>
-            <Link
-              href="/#inf02"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/#inf02')}
-            >
-              INF.02/EE.08
-            </Link>
-            <Link
-              href="/#inf03"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/#inf03')}
-            >
-              INF.03/EE.09/E.14
-            </Link>
-            <Link
-              href="/search"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/search')}
-            >
-              Wyszukiwarka pytań
-            </Link>
-            <Link
-              href="/hardest"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/hardest')}
-            >
-              Najtrudniejsze pytania
-            </Link>
-            <Link
-              href="/privacy"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/privacy')}
-            >
-              Polityka prywatności
-            </Link>
-            <Link
-              href="/contact"
-              className="text-xl pb-2"
-              onClick={() => closeOnCurrent('/contact')}
-            >
-              Formularz kontaktowy
-            </Link>
-            <Link
-              href="https://github.com/maciejkrol18/teoriainformatyk"
-              target="_blank"
-              className="text-xl pb-2"
-            >
-              Github
-            </Link>
+                onClick={() => (document.body.style.overflow = 'auto')}
+              >
+                <Link href="/login">Zaloguj</Link>
+              </Button>
+            )}
           </div>
+          <Link href="/" className="text-xl pb-2" onClick={() => closeOnCurrent('/')}>
+            Strona główna
+          </Link>
+          <Link
+            href="/#inf02"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/#inf02')}
+          >
+            INF.02/EE.08
+          </Link>
+          <Link
+            href="/#inf03"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/#inf03')}
+          >
+            INF.03/EE.09/E.14
+          </Link>
+          <Link
+            href="/search"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/search')}
+          >
+            Wyszukiwarka pytań
+          </Link>
+          <Link
+            href="/hardest"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/hardest')}
+          >
+            Najtrudniejsze pytania
+          </Link>
+          <Link
+            href="/privacy"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/privacy')}
+          >
+            Polityka prywatności
+          </Link>
+          <Link
+            href="/contact"
+            className="text-xl pb-2"
+            onClick={() => closeOnCurrent('/contact')}
+          >
+            Formularz kontaktowy
+          </Link>
+          <Link
+            href="https://github.com/maciejkrol18/teoriainformatyk"
+            target="_blank"
+            className="text-xl pb-2"
+          >
+            Github
+          </Link>
         </nav>
       ) : null}
     </>
