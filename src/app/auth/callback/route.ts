ad0a4ce09132ea850615fb8e491c74fbda9c3498
Maciej Król <maciekgplay@gmail.com>
2024-10-21 23:43:55 +0200

    fix: handle reverse proxies in auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -4,14 +4,18 @@ import { createClient } from '@/lib/supabase/server'
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
 
   if (code) {
     const supabase = createClient()
     const { error } = await supabase.auth.exchangeCodeForSession(code)
     if (!error) {
-      return NextResponse.redirect(`${origin}${next}`)
+      const forwardedHost = request.headers.get('x-forwarded-host')
+      const isLocalEnv = process.env.NODE_ENV === 'development'
+      if (isLocalEnv || !forwardedHost) {
+        return NextResponse.redirect(`${origin}${next}`)
+      }
+      return NextResponse.redirect(`https://${forwardedHost}${next}`)
     }
   }
 
