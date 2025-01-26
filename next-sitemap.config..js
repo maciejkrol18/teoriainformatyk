73e42b701603939c5d425f653baaca01d306c16d
Maciej Król <maciekgplay@gmail.com>
2025-01-26 10:25:03 +0100

    feat: next-sitemap

---
+++ next-sitemap.config..js
@@ -0,0 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: `https://${process.env.PRODUCTION_URL}`,
+	generateRobotsTxt: true,
+	robotsTxtOptions: {
+		policies: [
+			{
+				userAgent: "*",
+				allow: "/",
+			},
+		],
+	},
+};
